eb7ff449d99032928048a23d945de6d229d836b7
MarcosPimienta <fenix3819@gmail.com>
2026-04-21 22:27:03 -0500

    feat: sync 3D model animations with store-based page navigation state

--- src/components/3d/ModelTest.tsx
+++ src/components/3d/ModelTest.tsx
@@ -1,6 +1,7 @@
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { useBookStore } from "../../store/useBookStore";
 
 export const ModelTest = () => {
   const coverGroup = useRef<THREE.Group>(null);
@@ -12,14 +13,25 @@ export const ModelTest = () => {
   const { actions: coverActions } = useAnimations(cover.animations, coverGroup);
   const { actions: pageActions } = useAnimations(page00.animations, pageGroup);
 
+  const currentPage = useBookStore((state) => state.currentPage);
+
   useEffect(() => {
-    if (coverActions["Animation"]) {
-      coverActions["Animation"].play();
+    if (currentPage === 1) {
+      if (coverActions["Animation"]) {
+        coverActions["Animation"].reset().fadeIn(0.5).play();
+        coverActions["Animation"].setLoop(THREE.LoopOnce, 1);
+        coverActions["Animation"].clampWhenFinished = true;
+      }
+      if (pageActions["Animation"]) {
+        pageActions["Animation"].reset().fadeIn(0.5).play();
+        pageActions["Animation"].setLoop(THREE.LoopOnce, 1);
+        pageActions["Animation"].clampWhenFinished = true;
+      }
+    } else if (currentPage === 0) {
+      if (coverActions["Animation"]) coverActions["Animation"].fadeOut(0.5).stop();
+      if (pageActions["Animation"]) pageActions["Animation"].fadeOut(0.5).stop();
     }
-    if (pageActions["Animation"]) {
-      pageActions["Animation"].play();
-    }
-  }, [coverActions, pageActions]);
+  }, [coverActions, pageActions, currentPage]);
 
   return (
     <group name="Model_Test">
